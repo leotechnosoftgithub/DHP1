7bd1824f9855b57739dc46abeb5169b9e0c7366e
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-06-04 13:25:13 +0530

    videoips1
    videoips1

--- mainurls/mimages.sh
+++ mainurls/mimages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dat=$1
 key=/root/.ssh/mst.pem
-curl -o ./ips/imageips  http://18.207.24.43/imageips
-for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/imageips`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha7.sh "\"$i\"" "\"$dat\"";done
+curl -o ./ips/imageips1  http://18.207.24.43/imageips1
+for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/imageips1`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha7.sh "\"$i\"" "\"$dat\"";done
 echo Subject: Job job1 completed | sendmail sheetal.pant@leotechnosoft.net
 screen -X quit
