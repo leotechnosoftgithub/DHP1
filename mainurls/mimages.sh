3b0eba923a1672ca273145970598c89158275620
leotechnosoftgithub <64788555+leotechnosoftgithub@users.noreply.github.com>
2020-07-26 13:38:01 +0530

    sdf
    sdf

--- mainurls/mimages.sh
+++ mainurls/mimages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 dat=$1
 key=/root/.ssh/mst.pem
-curl -o ./ips/imageips1  http://18.207.24.43/imageips1
+curl -o ./ips/imageips1  http://67.205.178.30/ips/imageips1
 for i in `cat xaa/gaa`;do ip=`shuf -n 1 ./ips/imageips1`; echo $ip; ssh -o StrictHostKeyChecking=no -i $key root@$ip ./alpha7.sh "\"$i\"" "\"$dat\"";done
 echo Subject: Job job1 completed | sendmail sheetal.pant@leotechnosoft.net
 screen -X quit
